621d8621616325af79ed1bf5ae599217b820f835
Fabiel Leon <fabiel.leon.oliva@gmail.com>
2020-12-08 02:36:13 -0500

    fix telegram channel name

--- src/telegramAPI.js
+++ src/telegramAPI.js
@@ -3,7 +3,7 @@ const { URLSearchParams } = require('url');
 
 module.exports = async (bot, channel, text, disablePreview = false, parseMode = 'html') => {
   const params = new URLSearchParams({
-    chat_id: `@${channel}`,
+    chat_id: channel,
     parse_mode: parseMode,
     disable_web_page_preview: disablePreview,
     text,
